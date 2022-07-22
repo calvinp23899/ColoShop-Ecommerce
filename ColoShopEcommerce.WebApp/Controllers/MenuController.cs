dfcfa714f908053974157c319098208b85a2f0f0
DESKTOP-57V1BME\ALIENWARE <calvinp2381999@gmail.com>
2022-07-22 15:38:05 +0700

    render and config router product

---
+++ ColoShopEcommerce.WebApp/Controllers/MenuController.cs
@@ -0,0 +1,40 @@
+﻿using ColoShopEcommerce.WebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ColoShopEcommerce.WebApp.Controllers
+{
+    public class MenuController : Controller
+    {
+        private ApplicationDbContext _dbContext = new ApplicationDbContext();
+        // GET: Menu
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult MenuTop()
+        {
+            var items = _dbContext.Categories.OrderBy(x => x.Position).ToList();
+            return PartialView("_MenuTop", items);
+        }
+
+        public ActionResult MenuProductCategory(int? id)
+        {
+            if(id != null)
+            {
+                ViewBag.CateId = id;
+            }
+            var items = _dbContext.ProductCategories.ToList();
+            return PartialView("_MenuProductCategory", items);
+        }
+
+        public ActionResult MenuNewArrival()
+        {
+            var items = _dbContext.ProductCategories.ToList().Take(3);
+            return PartialView("_MenuNewArrival", items);
+        }
+    }
+}
---
+++ ColoShopEcommerce.WebApp/Controllers/ProductController.cs
@@ -0,0 +1,53 @@
+﻿using ColoShopEcommerce.WebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ColoShopEcommerce.WebApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        private ApplicationDbContext _dbContext = new ApplicationDbContext();
+        // GET: CategoryProduct
+        public ActionResult Index(int? id)
+        {
+            var items = _dbContext.Products.ToList();
+            if(id != null)
+            {
+                items = items.Where(x=>x.ProductCategoryID == id).ToList();
+            }
+            return View(items);
+        }
+
+        public ActionResult ProductCategory(string alias,int? id)
+        {
+            var items = _dbContext.Products.ToList();
+            if (id != null)
+            {
+                items = items.Where(x => x.ProductCategoryID == id).ToList();
+            }
+            ViewBag.CateId = id;
+            return View(items);
+        }
+
+        public ActionResult ProductByCateId(int cateId)
+        {
+            var items = _dbContext.Products.Where(x=>x.ProductCategoryID == cateId).ToList().Take(6);
+            return PartialView("_ProductByCateId", items);
+        }
+
+        public ActionResult ProductByCateNewArrival()
+        {
+            var items = _dbContext.Products.Where(x=>x.IsActive).Take(12).ToList();
+            return PartialView("_ProductByCateNewArrival", items);
+        }
+
+        public ActionResult HomeBestSeller()
+        {
+            var items = _dbContext.Products.Where(x=>x.IsActive && x.IsHot).Take(12).ToList();
+            return PartialView("_HomeBestSeller", items);
+        }
+    }
+}
