94e1d35deeb1041a99a5e7d87b0d543ed06e3c47
DESKTOP-57V1BME\ALIENWARE <calvinp2381999@gmail.com>
2022-07-24 12:49:13 +0700

    add to cart

---
+++ ColoShopEcommerce.WebApp/Models/Cart.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ColoShopEcommerce.WebApp.Models
+{
+    public class Cart
+    {
+        public List<CartItem> CartItems {get; set;}
+        public Cart()
+        {
+            this.CartItems = new List<CartItem>();
+        }
+
+        public void AddToCart(CartItem item,int Quantity)
+        {
+            var check = CartItems.FirstOrDefault(x => x.ProductId == item.ProductId);
+            if(check != null)
+            {
+                check.Quantity += Quantity;
+                check.TotalPrice = check.Price * check.Quantity;
+            }
+            else
+            {
+                CartItems.Add(item);
+            }
+        }
+
+        public void Remove(int id)
+        {
+            var checkExist = CartItems.SingleOrDefault(x=>x.ProductId == id);
+            if (checkExist != null)
+            {
+                CartItems.Remove(checkExist);
+            }
+
+        }
+
+        public void UpdateQuantity(int id, int Quantity)
+        {
+            var checkExist = CartItems.SingleOrDefault(x => x.ProductId == id);
+            if (checkExist != null)
+            {
+                checkExist.Quantity = Quantity;
+                checkExist.TotalPrice = checkExist.Price * checkExist.Quantity;
+            }
+        }
+
+        public decimal GetTotalPrice()
+        {
+            return CartItems.Sum(x => x.TotalPrice);
+        }
+
+        public int GetTotalQuantity()
+        {
+            return CartItems.Sum(x => x.Quantity);
+        }
+
+        public void ClearCart()
+        {
+            CartItems.Clear();
+        }
+    }
+
+    public class CartItem
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string Image { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public decimal TotalPrice { get; set; }
+    }
+}
---
+++ ColoShopEcommerce.WebApp/Scripts/jsShopping.js
@@ -0,0 +1,24 @@
+﻿$(document).ready(function () {
+    $('body').on('click', '.btnAddToCart', function (e) {
+        e.preventDefault(); // turn off href
+        var id = $(this).data('id');
+        var quantity = 1;
+        var Quantity = $('#quantity_value').text()
+        if (Quantity != '') {
+            quantity = parseInt(Quantity)
+        }
+        var sl = parseInt($('#quantity_value').text());
+        alert(id + " " + quantity);
+        $.ajax({
+            url: '/shoppingcart/addtocart',
+            type: 'POST',
+            data: { id: id, quantity: quantity },
+            success: function (rs) {
+                if (rs.Success) {
+                    $('#checkout_items').html(rs.Count);
+                    alert(rs.msg);
+                }
+            }
+        });
+    });
+});
