74cc96fc8c529c954b87ba15043cc5967aba1cf9
DESKTOP-57V1BME\ALIENWARE <calvinp2381999@gmail.com>
2022-07-27 17:16:08 +0700

    ShoppingCart

--- ColoShopEcommerce.WebApp/Scripts/jsShopping.js
+++ ColoShopEcommerce.WebApp/Scripts/jsShopping.js
@@ -1,4 +1,5 @@
 ﻿$(document).ready(function () {
+    ShowCount();
     $('body').on('click', '.btnAddToCart', function (e) {
         e.preventDefault(); // turn off href
         var id = $(this).data('id');
@@ -8,7 +9,6 @@
             quantity = parseInt(Quantity)
         }
         var sl = parseInt($('#quantity_value').text());
-        alert(id + " " + quantity);
         $.ajax({
             url: '/shoppingcart/addtocart',
             type: 'POST',
@@ -16,9 +16,37 @@
             success: function (rs) {
                 if (rs.Success) {
                     $('#checkout_items').html(rs.Count);
-                    alert(rs.msg);
                 }
             }
         });
     });
+
+    $('body').on('click', '.btnDelete', function (e) {
+        e.preventDefault();
+        var id = $(this).data('id');
+        var cofirmMsg = confirm('Bạn có muốn xóa sản phẩm này khỏi giỏ hàng không ?')
+        if (cofirmMsg == true) {
+            $.ajax({
+                url: '/shoppingcart/DeleteCartItem',
+                type: 'POST',
+                data: { id: id },
+                success: function (rs) {
+                    if (rs.Success) {
+                        $('#checkout_items').html(rs.Count);
+                        $('#trow_' + id).remove();
+                    }
+                }
+            });
+        }     
+    });
 });
+
+function ShowCount() {
+    $.ajax({
+        url: '/shoppingcart/ShowCount',
+        type: 'GET',
+        success: function (rs) {
+            $('#checkout_items').html(rs.Count);
+        }
+    });
+}
